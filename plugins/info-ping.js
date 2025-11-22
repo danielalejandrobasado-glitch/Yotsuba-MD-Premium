e441738eecdea05f7c07c6ee993aceff78813b56
danielalejandrobasado-glitch <danielalejandrobasado@gmail.com>
2025-11-22 15:41:06 -0400

    Actualizar info-ping.js

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -3,13 +3,13 @@ import { spawn, exec, execSync } from 'child_process'
 
 let handler = async (m, { conn }) => {
 let timestamp = speed()
-let sentMsg = await conn.reply(m.chat, '🎤💙 Sincronizando frecuencias virtuales... ✨', m)
+let sentMsg = await conn.reply(m.chat, '⚽🔥 Analizando visión directa... 🎯', m)
 let latency = speed() - timestamp
 exec(`neofetch --stdout`, (error, stdout, stderr) => {
 let child = stdout.toString("utf-8");
 let ssd = child.replace(/Memory:/, "Ram:")
 
-let result = `🎵✨ *¡Miku Pong!* ✨🎵\n💙 Latencia del Escenario Virtual ⴵ ${latency.toFixed(4).split(".")[0]}ms 💙\n${ssd}`
+let result = `⚽✨ *¡Isagi Pong!* 🔥⚽\n🎯 Tiempo de Reacción ⴵ ${latency.toFixed(4).split(".")[0]}ms 🎯\n${ssd}`
 conn.sendMessage(m.chat, { text: result, edit: sentMsg.key }, { quoted: m })
 })
 }
@@ -18,4 +18,3 @@ handler.tags = ['info']
 handler.command = ['ping', 'p']
 
 export default handler
-
