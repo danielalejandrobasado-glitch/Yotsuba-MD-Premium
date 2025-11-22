1e925a4a06ced383f39ebad1dce1059cc6f3f05d
danielalejandrobasado-glitch <danielalejandrobasado@gmail.com>
2025-11-22 11:46:06 -0400

    Actualizar descargas-instagram.js

--- plugins/descargas-instagram.js
+++ plugins/descargas-instagram.js
@@ -2,21 +2,21 @@ import { igdl } from 'ruhend-scraper';
 
 const handler = async (m, { args, conn }) => {
   if (!args[0]) {
-    return conn.reply(m.chat, `💙 Por favor, ingresa un enlace de Instagram para descargarlo en el mundo virtual 🎵`, m, rcanal);
+    return conn.reply(m.chat, `⚽ Por favor, ingresa un enlace de Instagram para descargarlo en el entrenamiento 🏃‍♂️`, m, rcanal);
   }
 
   try {
-    await m.react('🎤');
+    await m.react('⚽');
     const res = await igdl(args[0]);
     const data = res.data;
 
     for (let media of data) {
-      await conn.sendFile(m.chat, media.url, 'instagram.mp4', `💙 ¡Aquí tienes tu descarga del concierto virtual! 🎵`, m,rcanal);
+      await conn.sendFile(m.chat, media.url, 'instagram.mp4', `⚽ ¡Aquí tienes tu descarga de la estrategia! 🏃‍♂️`, m,rcanal);
     }
-    await m.react('✨');
+    await m.react('🔥');
   } catch (e) {
-    await m.react('💙');
-    return conn.reply(m.chat, `💙 ¡Gomen! Ocurrió un error en el mundo virtual. ¡Inténtalo de nuevo! 💫`, m, rcanal);
+    await m.react('⚽');
+    return conn.reply(m.chat, `⚽ ¡Error en la jugada! Ocurrió un problema en el campo. ¡Inténtalo de nuevo! 🔥`, m, rcanal);
   }
 };
 
