5dd0e1b47c3805242df84fe32f3bd09b046e2f3e
PixelCrew Team <pixelcrewteam@gmail.com>
2025-11-23 19:57:11 -0400

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -109,14 +109,13 @@ global.rcanal = {
 
 global.redes = 'https://whatsapp.com/channel/0029Vb73g1r1NCrTbefbFQ2T'
 global.dev = '© 🄿🄾🅆🄴🅁🄴🄳 DuarteXV'
-
 global.emoji = '⚽️'
 global.emoji2 = '🏆'
 global.emoji3 = '🔥'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.catalogo = fs.readFileSync('./src/catalogo.jpg');
+global.catalogo = 'mienlace'
 global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
 global.ch = {
 ch1: '120363420979328566@newsletter',
@@ -131,18 +130,11 @@ global.fetch = fetch
 global.axios = axios
 global.moment = moment
 
-
-
-
-
 global.opts = {
   ...global.opts,
   autoread: true,  
   queque: false 
 }
-
-
- 
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
