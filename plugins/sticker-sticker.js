82398d61f927e955a9ddc2e065acc222476247b1
danielalejandrobasado-glitch <danielalejandrobasado@gmail.com>
2025-11-23 13:27:20 -0400

    Actualizar sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -16,9 +16,9 @@ let txt = args.join(' ')
 
 if (/webp|image|video/g.test(mime) && q.download) {
 if (/video/.test(mime) && (q.msg || q).seconds > 16)
-return conn.reply(m.chat, '💙 El video no puede durar más de *15 segundos* para crear un sticker virtual perfecto ✨', m, global.rcanal)
+return conn.reply(m.chat, '⚽️ El video no puede durar más de *15 segundos* para crear un sticker perfecto ✨', m, global.rcanal)
 let buffer = await q.download()
-await m.react('🎤')
+await m.react('🔥')
 
 let marca = txt ? txt.split(/[\u2022|]/).map(part => part.trim()) : [texto1, texto2]
 stiker = await sticker(buffer, false, marca[0], marca[1])
@@ -26,14 +26,14 @@ stiker = await sticker(buffer, false, marca[0], marca[1])
 let buffer = await sticker(false, args[0], texto1, texto2)
 stiker = buffer
 } else {
-return conn.reply(m.chat, '💙 Por favor, envía una *imagen* o *video* para crear un hermoso sticker virtual ✨', m, global.rcanal)
+return conn.reply(m.chat, '⚽️ Por favor, envía una *imagen* o *video* para crear un hermoso sticker virtual ✨', m, global.rcanal)
 }} catch (e) {
-await conn.reply(m.chat, '💫 ¡Gomen! Ocurrió un error en el escenario virtual: ' + e.message + ' ✨', m, global.rcanal)
-await m.react('🎵')
+await conn.reply(m.chat, '💫 ¡Error en la jugada! Ocurrió un error en el campo: ' + e.message + ' ✨', m, global.rcanal)
+await m.react('🔥')
 } finally {
 if (stiker) {
 conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
-await m.react('💙')
+await m.react('⚽️')
 }}}
 
 handler.help = ['sticker']
