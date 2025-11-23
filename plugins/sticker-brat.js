c14f4258c64f2e86e79398d7a5060712edec93f7
danielalejandrobasado-glitch <danielalejandrobasado@gmail.com>
2025-11-23 13:13:10 -0400

    Actualizar sticker-brat.js

--- plugins/sticker-brat.js
+++ plugins/sticker-brat.js
@@ -25,7 +25,7 @@ let handler = async (m, { conn, text }) => {
         text = m.quoted.text
     } else if (!text) {
         return conn.sendMessage(m.chat, {
-            text: `💙 Por favor, responde a un mensaje o ingresa un texto para crear el Sticker.`,
+            text: `⚽️ Por favor, responde a un mensaje o ingresa un texto para crear el Sticker.`,
         }, { quoted: m })
     }
 
@@ -41,7 +41,7 @@ let handler = async (m, { conn, text }) => {
         if (stiker) {
             return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
         } else {
-            throw new Error("💙 No se pudo generar el sticker.")
+            throw new Error("⚽️ No se pudo generar el sticker.")
         }
     } catch (error) {
         return conn.sendMessage(m.chat, {
