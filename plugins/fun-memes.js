346c75ff8353dee1ce7a71b6b49b0b21ebdbc85b
danielalejandrobasado-glitch <danielalejandrobasado@gmail.com>
2025-11-24 08:22:00 -0400

    Actualizar fun-memes.js

--- plugins/fun-memes.js
+++ plugins/fun-memes.js
@@ -2,7 +2,7 @@ import hispamemes from 'hispamemes'
 let handler = async (m, { conn, usedPrefix, command }) => {
 const meme = hispamemes.meme()
 conn.sendFile(m.chat, meme, '', '', m)
-m.react('🎤')
+m.react('⚽️')
 }
 handler.help = ['meme']
 handler.tags = ['fun']
